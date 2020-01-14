10ee9809faff5af0c958904c928ee9382799ce5f
pigwidgeondeluxe <15840839+PigwidgeonDeluxe@users.noreply.github.com>
2020-01-14 16:10:45 -0500

    tut-2: completed, displays a bmp of a sunflower for 2 secs

--- LazyFoosTutorial/Main.cpp
+++ LazyFoosTutorial/Main.cpp
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 // windows dimensions
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 600;
+const int SCREEN_HEIGHT = 600;
 
 //// Declared functions
 // starts SDL and creates window
@@ -26,7 +26,99 @@ SDL_Surface* gHelloWorld = NULL;
 // main function
 int main(int argc, char* args[])
 {
-	
+	// Start up SDL and create window
+		if (!init())
+		{
+			printf("Failed to initialize!\n");
+		}
+		else
+		{
+			//Load media
+			if (!loadMedia())
+			{
+				printf("Failed to load media!\n");
+			}
+			else
+			{
+				//Apply the image
+				SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
 
-	return 0;
+				//Update the surface
+				SDL_UpdateWindowSurface(gWindow);
+
+				//Wait two seconds
+				SDL_Delay(2000);
+			}
+		}
+
+		//Free resources and close SDL
+		close();
+
+		return 0;
+}
+
+// initialization function
+bool init()
+{
+	//init flag
+	bool success = true;
+
+	//initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL could not initialize. SDL_Error: %s\n", SDL_GetError());
+		success = false;
+	}
+	else
+	{
+		// creat window
+		gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (gWindow == NULL)
+		{
+			printf("Window not created. SDL_Error: %s\n", SDL_GetError());
+			success = false;
+		}
+		else
+		{
+			// get window surface
+			gScreenSurface = SDL_GetWindowSurface(gWindow);
+		}
+	}
+
+	return success;
+
+}
+
+// function to load our image
+bool loadMedia()
+{
+	//Loading success flag
+	bool success = true;
+
+	char filePath[] = "tut_2/image.bmp";
+
+	//Load splash image
+	gHelloWorld = SDL_LoadBMP(filePath);
+	if (gHelloWorld == NULL)
+	{
+		printf("Unable to load image %s! SDL Error: %s\n", filePath, SDL_GetError());
+		success = false;
+	}
+
+	return success;
+}
+
+// function to close the window
+void close()
+{
+	//Deallocate surface
+	SDL_FreeSurface(gHelloWorld);
+	gHelloWorld = NULL;
+
+	//Destroy window
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+
+	//Quit SDL subsystems
+	SDL_Quit();
 }
